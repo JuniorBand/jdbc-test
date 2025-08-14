78036a3ec9f468775afb3ebcae98d97326950a61
Júnior Bandeira <bandeirajunior150@gmail.com>
2025-08-13 21:36:01 -0300

    findById implementation

--- src/demo_dao_jdbc/application/Program.java
+++ src/demo_dao_jdbc/application/Program.java
@@ -1,5 +1,6 @@
 package demo_dao_jdbc.application;
 
+import demo_dao_jdbc.db.DB;
 import demo_dao_jdbc.model.dao.DaoFactory;
 import demo_dao_jdbc.model.dao.DepartmentDao;
 import demo_dao_jdbc.model.dao.SellerDao;
@@ -8,6 +9,7 @@ import demo_dao_jdbc.model.dao.impl.SellerDaoJDBC;
 import demo_dao_jdbc.model.entities.Department;
 import demo_dao_jdbc.model.entities.Seller;
 
+import java.sql.Connection;
 import java.util.Date;
 
 
@@ -15,12 +17,16 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department dep = new Department(1, "Books");
-        Seller sel = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
-        System.out.println(dep + "\n"+ sel);
+//        Department dep = new Department(1, "Books");
+//        Seller sel = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
 
-        SellerDao sellerDao = DaoFactory.createSellerDao(new SellerDaoJDBC());
+        SellerDao sellerDao = DaoFactory.createSellerDao(new SellerDaoJDBC(DB.getConnection()));
+
+        Seller sel = sellerDao.findById(3);
+
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao(new DepartmentDaoJDBC());
+
+        System.out.println(sel);
 
 
     }
--- src/demo_dao_jdbc/model/dao/impl/SellerDaoJDBC.java
+++ src/demo_dao_jdbc/model/dao/impl/SellerDaoJDBC.java
@@ -1,19 +1,34 @@
 package demo_dao_jdbc.model.dao.impl;
 
+import demo_dao_jdbc.db.DB;
+import demo_dao_jdbc.db.DbException;
+import demo_dao_jdbc.model.dao.DepartmentDao;
 import demo_dao_jdbc.model.dao.SellerDao;
+import demo_dao_jdbc.model.entities.Department;
 import demo_dao_jdbc.model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
 
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+
     @Override
-    public void insert(Seller obj) {
+    public void insert(Seller sel) {
 
     }
 
     @Override
-    public void update(Seller obj) {
+    public void update(Seller sel) {
 
     }
 
@@ -25,7 +40,47 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public Seller findById(Integer id) {
 
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT sellers.*,departments.Name as DepName " +
+                            "FROM sellers INNER JOIN departments " +
+                            "ON sellers.DepartmentId = departments.id " +
+                            "WHERE sellers.Id = ?");
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+                Department dep = new Department();
+                // Assuming Department has a constructor that takes id and name
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+
+                // Assuming Seller has a constructor that takes all necessary fields
+                Seller sel = new Seller();
+
+                sel.setId(rs.getInt("Id"));
+                sel.setName(rs.getString("Name"));
+                sel.setEmail(rs.getString("Email"));
+                sel.setBirthDate(rs.getDate("BirthDate"));
+                sel.setBaseSalary(rs.getDouble("Salary"));
+
+                sel.setDepartment(dep);
+
+                return sel;
+            } else {
+                return null; // No seller found with the given id
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+        }
     }
 
     @Override
